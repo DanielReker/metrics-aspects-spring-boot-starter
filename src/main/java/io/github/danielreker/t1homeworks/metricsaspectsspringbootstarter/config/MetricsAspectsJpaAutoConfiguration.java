79fadb3102c7f433f364041fac84b672b7073630
DanielReker <sasha29072004@gmail.com>
2025-06-16 19:09:50 +0400

    Refactor configuration

--- src/main/java/io/github/danielreker/t1homeworks/metricsaspectsspringbootstarter/config/MetricsAspectsJpaAutoConfiguration.java
+++ src/main/java/io/github/danielreker/t1homeworks/metricsaspectsspringbootstarter/config/MetricsAspectsJpaAutoConfiguration.java
@@ -1,9 +1,12 @@
 package io.github.danielreker.t1homeworks.metricsaspectsspringbootstarter.config;
 
 import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @AutoConfiguration
+@ConditionalOnClass(JpaRepository.class)
 @Import({ MetricsAspectsJpaRegistrar.class })
 public class MetricsAspectsJpaAutoConfiguration {
 }
