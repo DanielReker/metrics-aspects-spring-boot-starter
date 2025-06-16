88acdb219ec5fa91c62e841e09f811195614f33d
DanielReker <sasha29072004@gmail.com>
2025-06-16 20:25:34 +0400

    Remove duplicated @EnableConfigurationProperties

--- src/main/java/io/github/danielreker/t1homeworks/metricsaspectsspringbootstarter/config/MetricsAspectsStarterKafkaAutoConfiguration.java
+++ src/main/java/io/github/danielreker/t1homeworks/metricsaspectsspringbootstarter/config/MetricsAspectsStarterKafkaAutoConfiguration.java
@@ -6,12 +6,10 @@ import io.github.danielreker.t1homeworks.metricsaspectsspringbootstarter.kafka.M
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @AutoConfiguration
-@EnableConfigurationProperties(MetricsAspectsProperties.class)
 @ConditionalOnBean({ KafkaTemplate.class })
 @ConditionalOnAspectsMetricsEnabled
 public class MetricsAspectsStarterKafkaAutoConfiguration {
